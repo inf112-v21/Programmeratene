dda87a6923d487b6f32d3581d77b6c017abb2b6f
Jan Erik Syltøy <jan_erik00@hotmail.com>
2021-02-17 13:48:03 +0100

    Add Card-classes and Deck-class with generator

---
+++ src/main/java/card/CardMove.java
@@ -0,0 +1,26 @@
+package card;
+
+/**
+ * Moves robot
+ * -1 = 1 step back
+ * 1 = 1 step forward
+ * 2 = 2 step forward
+ * 3 = 3 step forward
+ */
+public class CardMove implements ICard{
+    int steps;
+    int priority;
+
+    public CardMove(int steps, int priority){
+        this.steps = steps;
+        this.priority = priority;
+    }
+
+    /**
+     * @return priority value of the card as an int
+     */
+    @Override
+    public int getPriority() {
+        return priority;
+    }
+}
---
+++ src/main/java/card/Deck.java
@@ -0,0 +1,33 @@
+package card;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+    ArrayList<ICard> cards;
+
+    public Deck(){
+        generateFullDeck();
+    }
+
+    void generateFullDeck(){
+        for(int i=0; i<18; i++){
+            cards.add(new CardMove(1, 490 + 10*i)); //move 1
+            cards.add(new CardTurn(-1, 70 + 20*i)); //rotate left
+            cards.add(new CardTurn(1, 80 + 20*i)); //rotate right
+        }
+        for(int i=0; i<12; i++){
+            cards.add(new CardMove(2, 670 + 10*i)); //move 2
+        }
+        for(int i=0; i<6; i++){
+            cards.add(new CardMove(3, 790 + 10*i)); //move 3
+            cards.add(new CardMove(-1, 430 + 10*i)); //move backwards 1
+            cards.add(new CardTurn(2, 10 + 10*i)); //rotate 180deg
+        }
+    }
+
+    void shuffle(){
+        Collections.shuffle(cards);
+    }
+
+}
