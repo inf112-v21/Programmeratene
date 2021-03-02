7748c40e7c034e21677175d1e6bc93131456dbcd
Jan Erik Syltøy <jan_erik00@hotmail.com>
2021-03-02 17:34:13 +0100

    Ask user for input when running Main.main()

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,11 +4,20 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import game.GUI;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
         cfg.setTitle("RoboRally");
         cfg.setWindowedMode(500, 500);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Start game as host? (y/n): ");
+
+        if (sc.nextLine().equals("y")){
+
+        }
 
         new Lwjgl3Application(new GUI(), cfg);
     }
