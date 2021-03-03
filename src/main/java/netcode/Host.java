b3d0533444307b6ba3aef43a0108e1712628fa9c
Jan Erik Syltøy <jan_erik00@hotmail.com>
2021-03-03 16:55:59 +0100

    (Fellesprog. alle) Fjernet ubrukt import

--- src/main/java/netcode/Host.java
+++ src/main/java/netcode/Host.java
@@ -5,7 +5,6 @@ import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class Host extends Listener {
