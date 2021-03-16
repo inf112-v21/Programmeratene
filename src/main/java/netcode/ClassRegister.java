69971e1efbec5268fb0b0706ecee2a7169379bdf
Jan Erik Syltøy <jan_erik00@hotmail.com>
2021-03-16 13:26:53 +0100

    added new packet to ClassRegister

--- src/main/java/netcode/ClassRegister.java
+++ src/main/java/netcode/ClassRegister.java
@@ -4,6 +4,7 @@ import card.CardMove;
 import card.CardTurn;
 import card.ICard;
 import com.esotericsoftware.kryo.Kryo;
+import netcode.packets.AddPlayerPacket;
 import netcode.packets.CardListPacket;
 
 import java.util.ArrayList;
@@ -12,6 +13,7 @@ public class ClassRegister {
 
     static void registerAll(Kryo k){
         k.register(CardListPacket.class);
+        k.register(AddPlayerPacket.class);
         k.register(ArrayList.class);
         k.register(ICard.class);
         k.register(CardMove.class);
